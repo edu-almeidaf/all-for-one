b6be1fb9aa0fa45106f81b7179c077810519f670
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-31 20:06:12 -0300

    refactor requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,3 @@
 SELECT notes FROM purchase_orders
-WHERE notes >= 'Purchase generated based on Order #30'
-AND notes <= 'Purchase generated based on Order #39';
+WHERE notes BETWEEN 'Purchase generated based on Order #30'
+AND 'Purchase generated based on Order #39';
